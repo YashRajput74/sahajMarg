a912aa3f249d1ccb33cede36161b13a629668bfa
Yash Rajput <yashrajput7455@gmail.com>
2025-11-22 22:08:34 +0530

    sequence typing

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -7,19 +7,30 @@ const Message = ({ type, name, text, avatar }) => {
     return (
         <div className={`message ${isUser ? "user-message" : "ai-message"}`}>
             {!isUser && (
-                <div className="avatar" style={{ backgroundImage: `url(${avatar})` }}></div>
+                <div
+                    className="avatar"
+                    style={{ backgroundImage: `url(${avatar})` }}
+                ></div>
             )}
 
             <div className="message-content">
                 <p className="message-name">{name}</p>
 
                 <p className={`message-bubble ${isUser ? "user-bubble" : "ai-bubble"}`}>
-                    <TypewriterText text={text} />
+                    {isUser ? (
+                        text
+                    ) : (
+                        <TypewriterText text={text} />
+                    )}
+
                 </p>
             </div>
 
             {isUser && (
-                <div className="avatar" style={{ backgroundImage: `url(${avatar})` }}></div>
+                <div
+                    className="avatar"
+                    style={{ backgroundImage: `url(${avatar})` }}
+                ></div>
             )}
         </div>
     );
